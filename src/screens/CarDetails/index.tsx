f1194b06fa127a7f427014387d6b79472307792b
lopessoftec <lopessoftec@gmail.com>
2022-01-04 23:46:02 -0300

    continuando a interface de detalhes

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -6,7 +6,16 @@ import { ImageSlider } from '../../components/ImageSlider';
 import {
   Container,
   Header,
-  CarImages
+  CarImages,
+  Content,
+  Details,
+  Descriptions,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
 } from './styles';
 
 export function CarDetails() {
@@ -21,6 +30,25 @@ export function CarDetails() {
             imageUrl={['https://pngimg.com/uploads/audi/audi_PNG99491.png']}
           />
         </CarImages>
+
+        <Content>
+          <Details>
+            <Descriptions>
+              <Brand>Lamborghini</Brand>
+              <Name>Huracan</Name>
+            </Descriptions>
+
+            <Rent>
+              <Period>Ao dia</Period>
+              <Price>R$ 580,00</Price>
+            </Rent>
+          </Details>
+
+          <About>
+          Este é automovel desportivo. Surgiu do lendario 
+          touro de lide idultado na praça Real Maestranza de Servilla.
+          </About>
+        </Content>
     </Container>
   );
 }
