f4eb0387b6d1a5b49402644e788a38c5a8d45470
lopessoftec <lopessoftec@gmail.com>
2022-04-26 21:56:35 -0300

    Estrutura da base de dados

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native'
 import { 
   StatusBar,
@@ -15,6 +15,8 @@ import { useAuth } from '../../hooks/auth';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 import { PasswordInput } from '../../components/PasswordInput';
+
+import { database } from '../../database';
 
 import {
   Container,
@@ -60,6 +62,16 @@ export function SignIn() {
   function handleNewAccount(){
     navigation.navigate('SignUpFirstStep');
   }
+
+  useEffect(() => {
+   async function loadData() {
+     const userCollection = database.get('users');
+     const users = await userCollection.query().fetch();
+     console.log(users);
+   }
+
+   loadData();
+  }, []);
 
   return (
     <KeyboardAvoidingView behavior='position' enabled>
