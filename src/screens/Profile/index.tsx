9cb4678fca8a033cf5acf4ab39d8eb90bef65deb
lopessoftec <lopessoftec@gmail.com>
2022-05-20 19:23:00 -0300

    Desenvolvido confirmacao de sign out

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -108,6 +108,23 @@ export function Profile() {
     }
   }
 
+  async function handleSignOut() {
+    Alert.alert(
+      'Tem certeza?',
+      'Se você sair, irá precisar de internet para conectar-se novamente.',
+      [
+        {
+          text: 'Cancelar',
+          onPress: () => { }
+        },
+        {
+          text: 'Sair',
+          onPress: () => signOut()
+        }
+      ]
+    )
+  }
+
   return (
     <KeyboardAvoidingView behavior="position" enabled>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
@@ -119,7 +136,7 @@ export function Profile() {
                 onPress={handleBack}
               />
               <HeaderTitle>Editar Perfil</HeaderTitle>
-              <LogoutButton onPress={signOut}>
+              <LogoutButton onPress={handleSignOut}>
                 <Feather
                   name="power"
                   size={24}
